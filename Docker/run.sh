31ed441c0a0824db3fcee42ccc80f9b02b8ce1b1
Josef-Beppe <75065544+Josef-Beppe@users.noreply.github.com>
2022-11-20 12:41:13 +0100

    Update run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -2,5 +2,6 @@
 
 repository="https://github.com/Josef-Beppe/sample_website_for_automation.git"
 
+# CACHEBUST has different value each time and therefore makes it possible to use cache on part of Dockerfile, but not all of it.
 docker-compose build --pull --build-arg CACHEBUST=$(date +%s) --build-arg repo=$repository
 docker-compose up -d
